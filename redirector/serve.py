edea67d0b43e8b1d75fa0322a3343986de11376b
Stewart Summers <stewart.summers@hotmail.com>
2024-06-26 12:42:20 +0000

    stable poc

--- redirector/serve.py
+++ redirector/serve.py
@@ -1,13 +1,18 @@
 import threading
+from time import sleep
 import websockets
 import websockets.sync.server
 from scapy.all import UDP, sniff
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+
+
+g_websocket: websockets.sync.server.ServerConnection = None
+g_stop_threads: bool = False
+
 
 @dataclass
 class Redirector:
-    websocket: websockets.WebSocketServerProtocol = None
-    ws_server: websockets.WebSocketServer = None
+    websocket: websockets.sync.server.ServerConnection = None
 
     def serve(self) -> None:
         print("Starting websocket server")
@@ -20,39 +25,80 @@ class Redirector:
             print(f"Error in serve: {repr(e)}")
 
     def handler(self, websocket):
+        global g_websocket, g_stop_threads
+        g_websocket = websocket
+        g_stop_threads = False
         try:
-            self.websocket = websocket
-            # dont close the connection
-            while True:
-                pass
+            print("Client connected...")
+            raw_listener = Raw_listener()
+            raw_listener_thread = threading.Thread(target=raw_listener.listen, daemon=True, args=('Listener', lambda: g_stop_threads))
+            raw_listener_thread.start()
+
+            refresh_thread = threading.Thread(target=raw_listener.refresh_ports, daemon=True, args=('Port Refresh', lambda: g_stop_threads))
+            refresh_thread.start()
+
         except websockets.ConnectionClosed:
             print("Connection closed")
+            g_websocket = None
+            g_stop_threads = True
+        except Exception as e:
+            print(f"Error in handler: {repr(e)}")
+            g_websocket = None
+            g_stop_threads = True
+        finally:
+            for thread in [raw_listener_thread, refresh_thread]:
+                thread.join()
 
 
 @dataclass
 class Raw_listener:
-    punch_ports: list[int]
-    ws: websockets.WebSocketServerProtocol
+    punch_ports: list[int] = field(default_factory=lambda: [10070, 10071, 10072, 10073, 10074, 10075])
 
-    def listen(self):
-        print("Client connected. Starting listener...")
+    def listen(self, t, stop):
+        print("Starting listener...")
         try:
-            sniff(lfilter=lambda x: x.haslayer(UDP) and x[UDP].dport == 3658,
-                  prn=lambda x: self.send_packet(x))
+            while True:
+                sniff(lfilter=lambda x: x.haslayer(UDP) and x[UDP].dport == 3658,
+                      prn=lambda x: self.send_packet(x), count=100)
+                if stop():
+                    print(f"Stopping {t} thread...")
+                    break
         except Exception as e:
             print(f"Error in listen: {repr(e)}")
-    
-    def send_packet(self, pkt):
-        try:
-            sport = str(pkt[UDP].sport)
 
+    def send_packet(self, pkt):
+        global g_stop_threads
+        try:
+            if isinstance(pkt, str):
+                g_websocket.send(pkt)
+                return
+
+            sport = str(pkt[UDP].sport)
             if sport in self.punch_ports:
                 return
-            
-            self.ws.send(sport)
+
+            g_websocket.send(sport)
             self.punch_ports.append(sport)
+        except websockets.ConnectionClosed:
+            g_stop_threads = True
         except Exception as e:
             print(f"Error in send_packet: {repr(e)}")
+
+    def refresh_ports(self, t, stop):
+        try:
+            while True:
+                sleep(15)
+                self.punch_ports.clear()
+                if stop():
+                    print(f"Stopping {t} thread...")
+                    break
+        except Exception as e:
+            print(f"Error in refresh_ports: {repr(e)}")
+
+    # on init, send the default ports ahead of time
+    def __post_init__(self):
+        for port in self.punch_ports:
+            self.send_packet(str(port))
 
 
 def main():
@@ -60,17 +106,13 @@ def main():
         redirector = Redirector()
         server_thread = threading.Thread(target=redirector.serve, daemon=True)
         server_thread.start()
-        while not redirector.websocket:
-            pass
-        raw_listener = Raw_listener(punch_ports=[], ws=redirector.websocket)
-        listener_thread = threading.Thread(target=raw_listener.listen, daemon=True)
-        listener_thread.start()
 
-        for thread in [server_thread, listener_thread]:
-            thread.join()
+        # This thread is going to serve forever, so we are blocking main here
+        server_thread.join()
 
     except KeyboardInterrupt:
         print("Exiting...")
+
 
 if __name__ == "__main__":
     main()
