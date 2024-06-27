607aebecb988041135c9ea8aeee0a587daf33dc5
tyler retzlaff <tyler@speartech.org>
2024-06-27 09:26:47 -0400

    adding logic for system os detection and using python method of running chromium-based browser

--- run.sh
+++ run.sh
@@ -2,27 +2,33 @@
 curr_dir=$(pwd -P) 
 echo "$curr_dir"
 # Check for Python installation
+
 if python3 --version &> /dev/null; then
     echo "Python3 is installed."
+    py_version='python3'
+else if python --version &> /dev/null; then
+    echo "Python is installed."
+    py_version='python'
 else
-    echo "Python3 is not installed. Please install Python before continuing."
+    echo "Python is not installed. Please install Python before continuing."
     exit 1
 fi
 
 
-# Launch default web browser with specified parameters
-echo "Launching default web browser..."
-# Check if OS is macOS or Linux and open Chrome accordingly
-if [[ $(uname) == "Darwin" ]]; then
-    open -na "Google Chrome" --args --disable-pinch --guest --disable-extensions --app="http://127.0.0.1:8000"
-elif [[ $(uname) == "Linux" ]]; then
-    google-chrome --disable-pinch --guest --disable-extensions --app="http://127.0.0.1:8000"
-else
-    echo "Unsupported operating system."
-    exit 1
-fi
+# # Launch default web browser with specified parameters
+# echo "Launching default web browser..."
+# # Check if OS is macOS or Linux and open Chrome accordingly
+# if [[ $(uname) == "Darwin" ]]; then
+#     open -na "Google Chrome" --args --disable-pinch --guest --disable-extensions --app="http://127.0.0.1:8000"
+# elif [[ $(uname) == "Linux" ]]; then
+#     google-chrome --disable-pinch --guest --disable-extensions --app="http://127.0.0.1:8000"
+# else
+#     echo "Unsupported operating system."
+#     exit 1
+# fi
+
 # Install Python Dependencies and Run Python script gui.py
 echo "Checking Python dependencies..."
-python3 -m pip install -r "$curr_dir/requirements.txt"
+$py_version -m pip install -r "$curr_dir/requirements.txt"
 echo "Running Python script gui.py..."
-python3 "$curr_dir/gui.py"
+$py_version "$curr_dir/gui.py"
