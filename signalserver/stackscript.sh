5dc35258e2329135375645ccd2718b32044e17f3
StewartS <31023869+airmon-ster@users.noreply.github.com>
2024-07-04 17:33:25 -0400

    stackscript working (#26)

--- signalserver/stackscript.sh
+++ signalserver/stackscript.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-#<UDF name="host" label="game host ip">
-# HOST=
+#<UDF name="host" label="game host ip"></UDF>
+# HOST=host
 apt-get update && apt-get upgrade -y
 apt install git python3-pip python3.12-venv -y
-git clone https://github.com/airmon-ster/SCCT-LAN-Breakout.git
-cd SCCT-LAN-Breakout/signalserver
-python3 -m venv ./.venv
-source ./.venv/bin/activate
+git clone https://github.com/airmon-ster/SCCT-LAN-Breakout.git /root/SCCT-LAN-Breakout
+cd /root/SCCT-LAN-Breakout/signalserver
+python3 -m venv /root/SCCT-LAN-Breakout/signalserver/.venv
+source /root/SCCT-LAN-Breakout/signalserver/.venv/bin/activate
 pip install -r requirements.txt
-cp scct.service /etc/systemd/system/
+cp /root/SCCT-LAN-Breakout/signalserver/scct.service /etc/systemd/system/
+systemctl start scct
 systemctl enable scct
 sysctl net.ipv4.ip_forward=1
 iptables -t nat -A PREROUTING -p udp --dport 3658 -j DNAT --to-destination $HOST
